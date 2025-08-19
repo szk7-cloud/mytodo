a5f8b90a1eb21bf199b7f9050d6c133c0936f7c5
szk7-cloud <am0zk.7@gmail.com>
2025-08-19 22:02:40 +0900

    first commit

---
+++ tasks/urls.py
@@ -0,0 +1,8 @@
+# tasks/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.task_list, name='task_list'),
+]
---
+++ tasks/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+# tasks/views.py
+
+from django.shortcuts import render
+from .models import Task
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
